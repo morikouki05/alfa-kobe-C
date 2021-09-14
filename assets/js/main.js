a863d41f5e74c8f20481c1e862c74f68b0cd3989
morikouki05 <89885439+morikouki05@users.noreply.github.com>
2021-09-14 15:46:55 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '海',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'TREASURE',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: 'カイゾクセン',
           // q2: 'かかか',
           // q3: 'ききき',
         }
